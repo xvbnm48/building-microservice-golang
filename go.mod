7a2ed8c7d364df1a613c2684ad577c8182ecb7f2
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-04-02 22:42:34 +0700

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xvbnm48/building-microservice-golang
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World")
+		d, _ := ioutil.ReadAll(r.Body)
+
+		log.Printf("Data %s\n", d)
+	})
+
+	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Goodbye World")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
